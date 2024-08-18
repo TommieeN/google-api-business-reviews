f74a32da3016d2076ac05232be76d9b12a930dc9
TommyNguyen <timsnguyen@hotmail.com>
2024-08-18 14:57:59 -0700

    sort reviews by date

--- index.js
+++ index.js
@@ -3,27 +3,30 @@ const axios = require("axios");
 const dotenv = require("dotenv")
 const app = express();
 const port = process.env.port || 3000;
+const cors = require("cors")
 
 dotenv.config()
 
+app.use(cors())
 app.get("/place-details", async (req, res) => {
   try {
     const apiKey = process.env.GOOGLE_API_KEY
     const placeId = process.env.PLACE_ID
     url = `https://maps.googleapis.com/maps/api/place/details/json?fields=reviews&place_id=${placeId}&key=${apiKey}`
     const response = await axios.get(url)
-
     const reviews = response.data.result.reviews;
 
     // Filter reviews for 5 star ratings and text
-    const fiveStarReviewsWithText = reviews.filter(review => review.rating === 5 && review.text)
+    const fiveStarReviewsWithText = reviews
+    .filter(review => review.rating === 5 && review.text)
+    .sort((a, b) => new Date(b.time) - new Date (a.time))
 
     const formattedResponse = fiveStarReviewsWithText.map(review => ({
         author: review.author_name,
         rating: review.rating,
         text: review.text,
         time: review.relative_time_description,
-        profile_photo_url: review.profile_photo_url
+        profile_photo_url: review.profile_photo_url,
     }))
 
     res.json(formattedResponse)
